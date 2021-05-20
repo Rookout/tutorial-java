f6aced172e31414ddc639aa9f75d28c58620281f
Nir Yarden <niryar@gmail.com>
2021-05-20 13:46:13 +0300

    Tracing Demo // added tracing activity

---
+++ src/main/java/com/rookout/tutorial/TracingHandler.java
@@ -0,0 +1,34 @@
+package com.rookout.tutorial;
+
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class TracingHandler {
+    Tracer tracer;
+
+    public TracingHandler() {
+        tracer = io.opentracing.util.GlobalTracer.get();
+    }
+
+    public void createChildSpansActivity() throws InterruptedException {
+        this.openAndCloseChildSpan("sub-operation1");
+        this.openAndCloseChildSpan("sub-operation2");
+        this.openAndCloseChildSpan("sub-operation3");
+    }
+
+    public void openAndCloseChildSpan(String operationName) throws InterruptedException {
+        Span parentSpan = tracer.scopeManager().activeSpan();
+        Span newChildSpan = tracer.buildSpan(operationName).asChildOf(parentSpan).start();
+        tracer.scopeManager().activate(newChildSpan);
+
+        int randomNum = ThreadLocalRandom.current().nextInt(5, 20);
+        TimeUnit.MILLISECONDS.sleep(randomNum);
+
+        tracer.scopeManager().activate(parentSpan);
+        newChildSpan.finish();
+    }
+
+}
