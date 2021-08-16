e7776b1c17e338f79c3b4a9186541b1a7ab6da53
Nir Yarden <42871571+niryarden@users.noreply.github.com>
2021-08-16 10:14:48 +0300

    improve LiveLogger demo (#37)

--- src/main/java/com/rookout/tutorial/MdcLogEnhancerFilter.java
+++ src/main/java/com/rookout/tutorial/MdcLogEnhancerFilter.java
@@ -5,9 +5,23 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.UUID;
 
 @Component
 public class MdcLogEnhancerFilter implements Filter {
+    private static final ArrayList<String> userIdsPool = new ArrayList<String>(){{
+        add(generateUserID());
+        add(generateUserID());
+        add(generateUserID());
+    }};
+    private static final ArrayList<String> accountIdsPool = new ArrayList<String>(){{
+        add(UUID.randomUUID().toString());
+        add(UUID.randomUUID().toString());
+        add(UUID.randomUUID().toString());
+    }};
+
     @Override
     public void destroy() { }
 
@@ -17,8 +31,21 @@ public class MdcLogEnhancerFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException
     {
+        Random rand = new Random();
+        MDC.put("requestId", UUID.randomUUID().toString());
+        MDC.put("userId", userIdsPool.get(rand.nextInt(userIdsPool.size())));
+        MDC.put("accountId", accountIdsPool.get(rand.nextInt(accountIdsPool.size())));
+
         MDC.put("remoteHost", servletRequest.getRemoteHost());
         MDC.put("localAddress", servletRequest.getLocalAddr());
+        MDC.put("name", servletRequest.getLocalName());
+        MDC.put("port", String.valueOf(servletRequest.getLocalPort()));
+        MDC.put("encoding", servletRequest.getCharacterEncoding());
+
         filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    private static String generateUserID() {
+        return UUID.randomUUID().toString().replace("-", "").substring(0, 8);
     }
 }
